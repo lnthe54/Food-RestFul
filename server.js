9bbda041941837efb044c8f3cc1df7e54a5ee89b
TheLN <lengocthe54@gmail.com>
2022-10-12 17:24:18 +0700

    modify codes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./route/food-route');
+const routes = require('./router/food-route');
 
 mongoose.connect('mongodb+srv://TheLN:The541998a@food.qblwamq.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection
